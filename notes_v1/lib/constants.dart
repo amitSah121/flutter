bc9679e2244765c24134a6bff5af5b3520e22084
amitSah121 <125210246+amitSah121@users.noreply.github.com>
2024-11-21 19:16:51 +0530

    Update constants.dart

--- notes_v1/lib/constants.dart
+++ notes_v1/lib/constants.dart
@@ -1,26 +1,46 @@
 
 import 'dart:io' show Platform, Directory;
 
+import 'package:path_provider/path_provider.dart';
+
 const url = "https://lemonwears.com/api";//"http://localhost:8080";
 
 
-String? imagePathConstant  = ((){
-  String os = Platform.operatingSystem;
-  String? home = "";
-  Map<String, String> envVars = Platform.environment;
-  if (Platform.isMacOS) {
-    home = envVars['HOME'];
-  } else if (Platform.isLinux) {
-    home = envVars['HOME'];
-  } else if (Platform.isWindows) {
-    home = envVars['UserProfile'];
-  }
+String? imagePathConstant  = "";
 
+void initializeImagePath() {
+  Future.microtask(() async {
+    // print("helo");
+    // String os = Platform.operatingSystem;
+    String? home = "";
+    Map<String, String> envVars = Platform.environment;
+    
+    if (Platform.isMacOS) {
+      home = envVars['HOME'];
+    } else if (Platform.isLinux) {
+      home = envVars['HOME'];
+    } else if (Platform.isWindows) {
+      home = envVars['UserProfile'];
+    } else if (Platform.isAndroid) {
+      home = (await getApplicationDocumentsDirectory()).path;
+    }
 
-  Directory('$home/images').create(recursive: true); 
+    // Ensure the directory exists
+    Directory imageDir = Directory('$home/images');
+    if (!await imageDir.exists()) {
+      await imageDir.create(recursive: true);
+    }
 
-  return '$home/images';
-})();
+    // Assign the directory path to imagePathConstant
+    imagePathConstant = imageDir.path;
+    print(imagePathConstant);
+    print("Images directory created at: $imagePathConstant");
+
+    // You can return the path if needed
+    return imagePathConstant;
+  });
+}
+
 
 String getNoteConstant(username, password, modulus, exponent){
   return '$url/notes?username=$username&password=$password&modulus=$modulus&exponent=$exponent';
@@ -40,5 +60,6 @@ const notesUrl = "$url/notes";
 const mediaUrl = "$url/media";
 
 String getKey(username){
+  
   return '$url/test?username=$username';
 }
